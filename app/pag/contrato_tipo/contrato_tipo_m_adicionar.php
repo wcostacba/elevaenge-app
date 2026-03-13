77d6ebd19e8cc66bc9b78869ead77bd818e9e7d6
wcostacba <adm@nwctecnologia.com.br>
2026-03-13 11:38:10 -0400

    Modulo contrato (tipo)

---
+++ app/pag/contrato_tipo/contrato_tipo_m_adicionar.php
@@ -0,0 +1,77 @@
+<?php
+require_once("../../inc_cfg_geral.php");
+
+mysql_conecta();
+
+// acesso
+$idpagina = "QZ35YHV2MV";
+//valida_permissao($idpagina);
+
+// acao
+if(decodifica($_POST["ac"]) == "novo_contratotipo") {
+    // form
+    $nome = trim(addslashes($_POST["nome"]));
+    
+    // valida   
+    if(empty($nome)) {
+        $_SESSION["aviso"] = array("O nome é inválido");
+        redireciona($app_cfg["path_raiz"]."/pag/contrato_tipo/contrato_tipo_listar.php");
+    }
+	
+    // transacao
+    $sql = "BEGIN";
+    mysql_query($sql) or mysql_excecao($sql);
+            
+    $sql = "INSERT INTO contrato_tipo (nome) VALUES (".mysql_prepara($nome).")";
+    mysql_query($sql) or mysql_excecao($sql);
+    
+    mysql_log($sql);
+        
+    // transacao
+    $sql = "COMMIT";
+    mysql_query($sql) or mysql_excecao($sql);
+    
+    redireciona($app_cfg["path_raiz"]."/pag/contrato_tipo/contrato_tipo_listar.php");
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+    <!-- cfg head -->
+    <?php require_once("../../inc_cfg_head.php"); ?>
+</head>
+
+<body>
+    <div id="conteudo">
+        
+        <!-- conteudo -->
+        <div class="box-body">
+            <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <label>Nome</label>
+                        <input type="text" class="form-control" name="nome" required>
+                    </div>
+                </div>
+            
+                <button type="submit" class="hidden"></button>
+                <input type="hidden" value="<?php echo codifica("novo_contratotipo"); ?>" id="ac" name="ac" required>
+            </form>
+        </div>
+        <!-- ./conteudo -->
+        
+    </div>
+    
+    <script type="text/javascript">
+        $(document).ready(function() {
+            setTimeout(function () {
+                $("input[name='nome']").focus();
+            }, 500);
+        });
+    </script>
+
+    <!-- cfg rodape -->
+    <?php require_once("../../inc_cfg_rodape.php"); ?>
+</body>
+</html>
